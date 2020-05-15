297a0bf80ab9e44a5d10be491338a0567a35e1d2
Firman Budi Safrizal <Firman.Safrizal@ag-it.com>
2020-05-15 21:27:11 +0700

    test 8

--- src/main/java/com/example/demo/controller/Controller.java
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -3,15 +3,45 @@ package com.example.demo.controller;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+    private static boolean isHealthy = true;
 
     @GetMapping("/")
     public String hello() throws UnknownHostException {
         InetAddress ip = InetAddress.getLocalHost();
-        return "hello from server - "+ ip;
+        if(isHealthy){
+            return "hello from server - "+ ip + "- healthy";
+        } else {
+            return "hello from server - "+ ip + "- unhealthy";
+        }
+        
+    }
+
+    @GetMapping("/check")
+    public ResponseEntity<?> check() throws UnknownHostException {
+        if(isHealthy){
+            return ResponseEntity.status(HttpStatus.OK).body("Server is Healthy");
+            // return "Server is healthy";
+        } else {
+            return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body("Server is UnHealthy");
+        }
+    }
+
+    @GetMapping("/healthy")
+    public String healthy() {
+        isHealthy = true;
+        return ""+isHealthy;
+    }
+
+    @GetMapping("/unhealthy")
+    public String unhealthy() {
+        isHealthy = false;
+        return ""+isHealthy;
     }
 }
